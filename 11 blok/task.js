99280071eb3dcb248b8d31ae331bce5c1e9627c7
Bogdan Fesnyak <sabotaj717@gmail.com>
2022-09-08 19:14:23 +0300

    new block in maket, new block of study

---
+++ 11 blok/task.js
@@ -0,0 +1,40 @@
+// Создайте объект Date для даты: 20 февраля 2012 года, 3 часа 12 минут. Временная зона – местная.
+// Для вывода используйте alert.
+// let d = new Date();
+// d.setFullYear(2012,1,20);
+// d.setHours(3,12,0,0);
+// alert (d);
+// Напишите функцию getWeekDay(date), показывающую день недели в коротком формате: «ПН», «ВТ», «СР», «ЧТ», «ПТ», «СБ», «ВС».
+// Например:
+
+// let date = new Date(2012, 0, 3);  // 3 января 2012 года
+// function getWeekDay(date){
+//     let day = date.getDay();
+//     switch(day){
+//         case 0:
+//             alert ("ВС");
+//             break;
+//         case 1:
+//             alert ("ПН");
+//             break;
+//         case 2:
+//             alert ("ВТ");
+//             break;
+//         case 3:
+//             alert ("СР");
+//             break;
+//         case 4:
+//             alert ("ЧТ");
+//             break;
+//         case 5:
+//             alert ("ПТ");
+//             break;
+//         case 6:
+//             alert ("СБ");
+//             break;
+//         default:
+//             alert ("Что-то пошло не так");
+//             break;
+//     }
+// }
+// alert( getWeekDay(date) );        // нужно вывести "ВТ"
