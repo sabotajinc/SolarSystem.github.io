dec0562c10b4ea8500cca8edee8ad9fcff2cd9bd
Bogdan Fesnyak <sabotaj717@gmail.com>
2022-09-25 18:48:52 +0300

    Add carousel

--- maket/script.js
+++ maket/script.js
@@ -2,20 +2,48 @@ function Slider (selector){
     this.selector = selector;
     this.rootEl = document.querySelector(selector);
     this.listOfSlides = this.rootEl.querySelector('.slider__slides');
-    this.slides = Array.from(this.rootEl.querySelectorAll('.slider__slide'));
-    this.buttons = this.rootEl.querySelector('.slider__pages');
-    this.button = Array.from(this.buttons.querySelectorAll('.slider__page'));
-    
-
+    this.button = Array.from(this.rootEl.querySelectorAll('.slider__page'));
 }
-const sliders = new Slider('.motto');
+
 Slider.prototype.moveSlide = function (){
-    this.button[0].addEventListener("click", function transformation(){
-            return sliders.listOfSlides.style.transform = `translate(0%)`;
+    this.button[0].addEventListener("click", () => {
+            return this.listOfSlides.style.transform = `translate(0%)`;
         });
-    this.button[1].addEventListener("click", function transformation(){
-            return sliders.listOfSlides.style.transform = `translate(-100%)`;
+    this.button[1].addEventListener("click", () => {
+            return this.listOfSlides.style.transform = `translate(-100%)`;
         });
 };
+const sliders = new Slider('.motto');
 sliders.moveSlide();
-console.log( );
+
+function Carousel (selector){
+    this.selector = selector;
+    this.rootElem = document.querySelector(selector);
+    this.carouselList = this.rootElem.querySelector('.excurs__carousel-list');
+    this.prevButton = this.rootElem.querySelector('.excurs__carousel-btn-left');
+    this.nextButton = this.rootElem.querySelector('.excurs__carousel-btn-right');
+}
+
+Carousel.prototype.moveCarousel = function (){
+    let trans = 0;
+    this.nextButton.addEventListener("click", () => {
+        if(trans<=-30){
+            trans = -30;
+            return this.carouselList.style.transform = `translate(${trans}%)`;
+        }else{
+            trans -= 10;
+            return this.carouselList.style.transform = `translate(${trans}%)`;
+        }
+    });
+    this.prevButton.addEventListener("click", () => {
+        if(trans>=30){
+            trans = 30;
+            return this.carouselList.style.transform = `translate(${trans}%)`;
+        }else{
+            trans += 10;
+            return this.carouselList.style.transform = `translate(${trans}%)`;
+        }
+    }, {'once': false});
+}
+const carousel = new Carousel('.excurs__carousel');
+carousel.moveCarousel();
